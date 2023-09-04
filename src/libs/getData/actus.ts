c6ea0d71cf938527cdd762c37d671ce6cae4c86f
achtaitaipai <charles.cailleteau@gmail.com>
2023-09-04 18:08:14 +0200

    sort actus

--- src/libs/getData/actus.ts
+++ src/libs/getData/actus.ts
@@ -6,6 +6,17 @@ export const getActus = async (etablissement?: string) => {
     (el) =>
       etablissement === undefined || etablissement === el.data.etablissement
   );
-  // Todo sort by date
-  return filteredItems;
+  const sortedItems = filteredItems.sort((a, b) => {
+    const firstDate = getDate(a.data.date);
+    const secondDate = getDate(b.data.date);
+
+    return secondDate.getTime() - firstDate.getTime();
+  });
+
+  return sortedItems;
+};
+
+const getDate = (dateStr: string) => {
+  const [day, month, year] = dateStr.split("-");
+  return new Date(Number(year), Number(month) - 1, Number(day));
 };
